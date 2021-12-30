b30696e29dbff0435c115d551624552fbc79a27b
Huthayfa0 <huthifa0@gmail.com>
2021-12-30 22:16:51 +0200

    main

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from playsound import playsound
 from scipy.io.wavfile import read
 import numpy as np
 from part1 import encode
+
 encode("Hello World")
-fs,data=read("output.wav")
-data=data.astype(np.float32)
+fs, data = read("output.wav")
+data = data.astype(np.float32)
+duration = 0.040
+samples_count = int(fs * duration)
+characters_waves = [data[i:i+samples_count] for i in range(0,len(data),samples_count)]
+for wave in characters_waves:
+    pass
+
 
