f44a2f5e29cadea0172f5a80230120178d27c3bb
Ameer-bazzar1 <79475533+Ameer-bazzar1@users.noreply.github.com>
2021-12-31 00:13:55 +0200

    AmeerBazzar

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from playsound import playsound
+from matplotlib import pyplot as plt
 from scipy.io.wavfile import read
 import numpy as np
+from scipy.signal import find_peaks
+
 from part1 import encode
+from scipy.fft import fft, fftfreq
 
 encode("Hello World")
+print("\n")
 fs, data = read("output.wav")
-data = data.astype(np.float32)
+exit(0)
 duration = 0.040
 samples_count = int(fs * duration)
 characters_waves = [data[i:i+samples_count] for i in range(0,len(data),samples_count)]
 for wave in characters_waves:
-    pass
-
-
+    frequency = []
+    y=fft(wave,128)
+    y=abs(y[1:64])
+    peaks = find_peaks(y)
+    arraysort=np.sort(peaks[0])[::-1]
+    print(peaks)
+    for i in arraysort[0:4]:
+        frequency.append((i*(4000/64)))
+    print(frequency)
+    break
