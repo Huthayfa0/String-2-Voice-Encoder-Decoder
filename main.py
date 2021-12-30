db0b300c291db82b8051830acd79423f967a118e
Huthayfa0 <huthifa0@gmail.com>
2021-12-30 17:10:21 +0200

    part1 is ready

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
-import pyaudio
+from ctypes import *
+from playsound import playsound
+from scipy.io.wavfile import write
 import numpy as np
 
-p = pyaudio.PyAudio()
+ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
 
-volume = 0.5     # range [0.0, 1.0]
-fs = 44100       # sampling rate, Hz, must be integer
-duration = 3.0   # in seconds, may be float
-f = 440.0        # sine frequency, Hz, may be float
 
-# generate samples, note conversion to float32 array
-samples = (np.sin(2*np.pi*np.arange(fs*duration)*1000/fs)+
-           np.sin(2*np.pi*np.arange(fs*duration)*800/fs)+
-           np.sin(2*np.pi*np.arange(fs*duration)*1600/fs)+
-           np.sin(2*np.pi*np.arange(fs*duration)*200/fs)).astype(np.float32)
+def py_error_handler(filename, line, function, err, fmt):
+    pass
 
-# for paFloat32 sample values must be in range [-1.0, 1.0]
-stream = p.open(format=pyaudio.paFloat32,
-                channels=1,
-                rate=fs,
-                output=True)
 
-# play. May repeat with different volume values (if done interactively)
-stream.write(volume*samples)
+c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+asound = cdll.LoadLibrary('libasound.so')
+asound.snd_lib_error_set_handler(c_error_handler)
 
-stream.stop_stream()
-stream.close()
+fs = 8000
+duration = 0.040
+n = np.arange(fs * duration)
 
-p.terminate()
+sin_waves = [[(np.sin(2 * np.pi * n * x / fs)).astype(np.float32) for x in [100, 200]],
+             [(np.sin(2 * np.pi * n * x / fs)).astype(np.float32) for x in [400, 600, 1000]],
+             [(np.sin(2 * np.pi * n * x / fs)).astype(np.float32) for x in [800, 1200, 2000]],
+             [(np.sin(2 * np.pi * n * x / fs)).astype(np.float32) for x in [1600, 2400, 4000]]]
+string = input("Please enter the string you want to encode: ")
+samples = np.empty(shape=[0], dtype=np.float32)
+for c in string:
+    if c == ' ':
+        wave = sin_waves[0][0] + sin_waves[1][-1] + sin_waves[2][-1] + sin_waves[3][-1]
+    else:
+        wave = sin_waves[0][0 if (c.islower()) else 1]
+        x = ord(c.lower()) - ord('a')
+        wave += sin_waves[3][x % 3]
+        x //= 3
+        wave += sin_waves[2][x % 3]
+        x //= 3
+        wave += sin_waves[1][x % 3]
+    samples = np.concatenate((samples, wave), axis=0)
+samples = samples.astype(np.float32)
+amp=np.iinfo(np.int16).max
+data = amp * samples
+write("output.wav", fs, data.astype(np.int16))
+playsound("output.wav")
