8cb440129df0cfe4c959df4dd9a3c158cb7328ac
Ameer-bazzar1 <79475533+Ameer-bazzar1@users.noreply.github.com>
2021-12-31 14:47:12 +0200

    part2 fft done

--- main.py
+++ main.py
@@ -1,23 +1,67 @@
-import matplotlib.pyplot as plt
-from scipy.io.wavfile import read
 import numpy as np
+from scipy.io.wavfile import read
 from scipy.signal import find_peaks
 
 from part1 import encode
 from scipy.fft import fft, fftfreq
 
-encode("Hello World")
+encode("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz")
 print("\n")
 fs, data = read("output.wav")
 print(type(data))
 duration = 0.040
 samples_count = int(fs * duration)
 characters_waves = [data[i:i+samples_count]for i in range(0,len(data),samples_count)]
+characters = { "A":[200, 400, 800, 1600],
+               "B":[200, 400, 800, 2400],
+               "C": [200, 400, 800, 4000],
+               "D": [200 ,400 ,1200 ,1600],
+               "E":[200, 400, 1200 ,2400],
+               "F":[200, 400, 1200, 4000],
+               "G": [200, 400, 1600, 2000],
+               "H": [200, 400, 2000, 2400],
+                "I" :[200, 400, 2000, 4000],
+                "J" :[200, 600 ,800, 1600],
+                "K" :[200, 600 ,800, 2400],
+                "L" :[200, 600 ,800 ,4000],
+                "M":[200, 600, 1200, 1600],
+               "N" :[200 ,600, 1200 ,2400],
+                "O" :[200 ,600 ,1200 ,4000],
+                "P" :[200 ,600 ,1600,2000],
+                "Q" :[200 ,600 ,2000, 2400],
+                "R" :[200 ,600 ,2000 ,4000],
+                "S" :[200 , 800,1000, 1600],
+                "T" :[200, 800,1000, 2400],
+                "U" :[200 , 800,1000, 4000],
+                "V" :[200 ,1000 ,1200 ,1600],
+                "W" :[200, 1000 ,1200, 2400],
+                "X" :[200 ,1000, 1200, 4000],
+                "Y" :[200 ,1000 , 1600,2000],
+                "Z":[200 ,1000,2000 ,2400]}
+string = ""
 for wave in characters_waves:
+    keys=[]
     frequency = []
+    frequencychar = []
     y=fft(wave)
     y=abs(y[0:len(y)//2+10])
     peaks,_ = find_peaks(y,height=1)
     for i in peaks:
         frequency.append(i*25)
-    print(frequency)
+    flag=0
+    if frequency[0]==100:
+        flag=1
+        frequency[0] = 200
+    keys = [k for k, v in characters.items() if v == frequency]
+    if flag:
+        if keys==[] :
+            string+=" "
+        else:
+            string+=keys[0].lower()
+    else:
+        if keys==[] :
+            string+=" "
+        else:
+            string+=keys[0]
+
+print(string)
--- part1.py
+++ part1.py
@@ -1,19 +1,19 @@
-from playsound import playsound
 from scipy.io.wavfile import write
 import numpy as np
-
-
 def encode(string, output_file="output.wav", fs=8000, duration=0.040, encode_frequencies=None):
     if encode_frequencies is None:
         encode_frequencies = [[100, 200], [400, 600, 1000], [800, 1200, 2000], [1600, 2400, 4000]]
-    n = np.arange(fs * duration)
+    n = np.arange(fs * +duration)
     waves = [[(np.cos(2 * np.pi * n * x / fs)) for x in frequencies] for frequencies in encode_frequencies]
     samples = np.empty(shape=[0])
     for c in string:
         if c == ' ':
             wave = waves[0][0] + waves[1][-1] + waves[2][-1] + waves[3][-1]
         else:
-            wave = waves[0][0 if (c.islower()) else 1]*1
+            wave = waves[0][0
+            if (c.islower())
+            else 1
+                   ]*1
             x = ord(c.lower()) - ord('a')
             wave += waves[3][x % 3]
             x //= 3
@@ -22,4 +22,3 @@ def encode(string, output_file="output.wav", fs=8000, duration=0.040, encode_fre
             wave += waves[1][x % 3]
         samples = np.concatenate((samples, wave), axis=0)
     write(output_file, fs, samples)
-    playsound(output_file)
