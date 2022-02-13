11d35ec2089bf12a445b1d7830742b4b26c383c1
Ameer-bazzar1 <79475533+Ameer-bazzar1@users.noreply.github.com>
2022-02-13 18:45:45 +0200

    new

--- part2.py
+++ part2.py
@@ -5,35 +5,35 @@ from scipy.fft import fft
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import butter, lfilter
-encode("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWw Xx Yy Zz")
-fs, data = read("output.wav")
-duration = 0.040
-samples_count = int(fs * duration)
-characters_waves = [data[i:i + samples_count] for i in range(0, len(data), samples_count)]
-encode_frequencies = [[100, 200], [400, 600, 1000], [800, 1200, 2000], [1600, 2400, 4000]]
-fs = 8000.0
-df = 10
-order = 4
-nyq = fs * 0.5
-filter_frequency = 200
-decode_filters = [[butter(order,[(filter_frequency - df) / nyq,
-                           (filter_frequency + df) / nyq] if filter_frequency + df < nyq else(filter_frequency - df) / nyq,
-                          btype='band' if filter_frequency + df < nyq else 'hp') for filter_frequency in frequencies] for frequencies in encode_frequencies]
-string = ''
-for wave in characters_waves:
-    filtered_signals = [[lfilter(filter_frequency[0], filter_frequency[1], wave) for filter_frequency in frequencies]
-                        for frequencies in decode_filters]
-    values = [[np.sum(sig * sig) for sig in signals] for signals in filtered_signals]
-    indices = [max(range(len(v)), key=v.__getitem__) for v in values]
-    if (indices[1] == 2 and indices[2] == 2 and indices[3] == 2):
-        c = ' '
-    else:
-        c = 0
-        for i in range(1, len(indices)):
-            c *= len(values[i])
-            c += indices[i]
-        c = chr(ord('a') + c)
-        if indices[0]==1:
-            c=c.upper()
-    string += c
-print(string)
+def decoder(input):
+    fs, data = read(input)
+    duration = 0.040
+    samples_count = int(fs * duration)
+    characters_waves = [data[i:i + samples_count] for i in range(0, len(data), samples_count)]
+    encode_frequencies = [[100, 200], [400, 600, 1000], [800, 1200, 2000], [1600, 2400, 4000]]
+    fs = 8000.0
+    df = 10
+    order = 4
+    nyq = fs * 0.5
+    filter_frequency = 200
+    decode_filters = [[butter(order,[(filter_frequency - df) / nyq,
+                               (filter_frequency + df) / nyq] if filter_frequency + df < nyq else(filter_frequency - df) / nyq,
+                              btype='band' if filter_frequency + df < nyq else 'hp') for filter_frequency in frequencies] for frequencies in encode_frequencies]
+    string = ''
+    for wave in characters_waves:
+        filtered_signals = [[lfilter(filter_frequency[0], filter_frequency[1], wave) for filter_frequency in frequencies]
+                            for frequencies in decode_filters]
+        values = [[np.sum(sig * sig) for sig in signals] for signals in filtered_signals]
+        indices = [max(range(len(v)), key=v.__getitem__) for v in values]
+        if (indices[1] == 2 and indices[2] == 2 and indices[3] == 2):
+            c = ' '
+        else:
+            c = 0
+            for i in range(1, len(indices)):
+                c *= len(values[i])
+                c += indices[i]
+            c = chr(ord('a') + c)
+            if indices[0]==1:
+                c=c.upper()
+        string += c
+    print("The string in " + input + " is: " + string)
